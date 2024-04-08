4cfe2baf815709ccd349fb61eff87816e15c31ee
yunxizhu <92202876+yunxizhu@users.noreply.github.com>
2024-04-08 09:39:10 +0800

    Update main.go

--- main.go
+++ main.go
@@ -11,22 +11,22 @@ func main() {
 	refreshTokens := config.ConfigInstance.RefreshToken
 	bilibiliCookies := config.ConfigInstance.BilibiliCookie
 	jdCookies := config.ConfigInstance.JdCookie
-	/*
+	
 	if refreshTokens != "" {
 		refreshTokenList := strings.Split(refreshTokens, ",")
 		aliCloudDisk := &platform.AliCloudDisk{}
 		for _, refreshToken := range refreshTokenList {
-			aliCloudDisk.Run(pushPlusToken, refreshToken)
+			//aliCloudDisk.Run(pushPlusToken, refreshToken)
 		}
 	}
 	if bilibiliCookies != "" {
 		bilibiliCookieList := strings.Split(bilibiliCookies, ",")
 		bilibili := &platform.Bilibili{}
 		for _, bilibiliCookie := range bilibiliCookieList {
-			bilibili.Run(pushPlusToken, bilibiliCookie)
+			//bilibili.Run(pushPlusToken, bilibiliCookie)
 		}
 	}
- */
+ 
 	if jdCookies != "" {
 		jdCookiesList := strings.Split(jdCookies, ",")
 		jd := &platform.JD{}
